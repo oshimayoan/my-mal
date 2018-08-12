18afc39541d7cdf06bf7ca1f601a69997300327b
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2018-08-12 18:27:57 +0700

    Init project

---
+++ flow-typed/global.js
@@ -0,0 +1,6 @@
+// @flow
+
+import * as React from 'react';
+
+declare type ReactNode = React.Node;
+declare type ReactEl = React.Element<*>;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+// @flow strict
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {unregister} from './registerServiceWorker';
+
+let root = document.getElementById('root');
+if (root) {
+  ReactDOM.render(<App />, root);
+}
+unregister();
