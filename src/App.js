bfabb322bf05537d977a6a896ba4993339b54aaf
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2018-08-12 20:11:35 +0700

    For starter, lets just show 3 animes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
 // @flow strict
 
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import JikanAPI from 'jikanjs';
+import ImageFrame from './core-ui/ImageFrame';
 
-class App extends Component<{}> {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+type Season = 'spring' | 'summer' | 'fall' | 'winter';
+
+type AnimeType = 'TV' | 'OVA' | 'ONA' | 'Movie' | 'Special';
+
+type AnimeList = $Exact<{
+  id: number,
+  url: string,
+  title: string,
+  imageUri: string,
+  type: AnimeType,
+}>;
+
+type LoadSeasonResult = $Exact<{
+  request_cached: boolean,
+  request_hash: string,
+  season: Array<
+    $Exact<{
+      mal_id: number,
+      url: string,
+      title: string,
+      image_url: string,
+      type: AnimeType,
+    }>,
+  >,
+  season_name: string,
+  season_year: number,
+}>;
+
+type State = $Exact<{
+  isLoadAnimeList: boolean,
+  animeList: Array<AnimeList>,
+}>;
+
+class App extends Component<{}, State> {
+  state = {
+    isLoadAnimeList: true,
+    animeList: [],
+  };
+
+  componentDidMount() {
+    this._fetchSeasonalAnime(2018, 'summer');
   }
+
+  render() {
+    let {isLoadAnimeList, animeList} = this.state;
+    let animes: Array<ReactNode> = [];
+    switch (isLoadAnimeList) {
+      case true: {
+        return <div>Fetching data...</div>;
+      }
+      default: {
+        if (animeList.length < 1) {
+          return <div>Nothing here</div>;
+        }
+        for (let i = 0; i < 3; i++) {
+          animes = [
+            ...animes,
+            <ImageFrame
+              key={i}
+              imageUri={animeList[i].imageUri}
+              title={animeList[i].title}
+            />,
+          ];
+        }
+        return <div style={{display: 'flex', flex: 1}}>{animes}</div>;
+      }
+    }
+  }
+
+  _fetchSeasonalAnime = async (year: number, season: Season) => {
+    try {
+      let result: LoadSeasonResult = await JikanAPI.loadSeason(year, season);
+      this.setState({
+        animeList: result.season.map((anime) => ({
+          id: anime.mal_id,
+          url: anime.url,
+          title: anime.title,
+          imageUri: anime.image_url,
+          type: anime.type,
+        })),
+        isLoadAnimeList: false,
+      });
+    } catch (err) {
+      this.setState({isLoadAnimeList: false});
+    }
+  };
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,12 @@
 // @flow strict
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should render without problem', () => {
+    let component = renderer.create(<App />);
+    expect(component).toMatchSnapshot();
+  });
 });
